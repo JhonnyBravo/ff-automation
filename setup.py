182fb3c36613fbe04ca2065959611398303b3690
Jhonny Bravo <sanfranceshika5@gmail.com>
2016-06-07 01:57:29 +0900

    modified: setup.py
    modified attribute:
       name, version, description, url, install_requires, entry_points
    
    deleted attribute: dependency_links

--- setup.py
+++ setup.py
@@ -8,17 +8,20 @@ except IOError:
     long_description = ""
 
 setup(
-    name="python-template",
-    version="0.1",
-    description="Template of Python Project.",
+    name="ff-automation",
+    version="1.0",
+    description="Firefox automation utility.",
     author="Jhonny Bravo",
     author_email="sanfranceshika5@gmail.com",
-    url="https://github.com/JhonnyBravo/python-template.git",
+    url="https://github.com/JhonnyBravo/ff-automation.git",
     packages=find_packages(),
-    install_requires=[],
-    dependency_links=[],
+    install_requires=["selenium"],
     entry_points={
-        "console_scripts": []
+        "console_scripts": [
+            "get_content_by_css_selector=ps_firefox.bin.get_content_by_css_selector:main",
+            "get_content_by_tag_name=ps_firefox.bin.get_content_by_tag_name:main",
+            "get_content_by_xpath=ps_firefox.bin.get_content_by_xpath:main"
+        ]
     },
     long_description=long_description,
     classifiers=[
